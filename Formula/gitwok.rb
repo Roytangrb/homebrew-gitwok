b5368429562bfcdf9181b77bf31628e4ecb423c4
Roy Tang <roytang.rb@gmail.com>
2021-01-18 21:08:43 +0800

    remove comments

--- Formula/gitwok.rb
+++ Formula/gitwok.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gitwok < Formula
   desc "Configurable CLI with conventional commits, changelog, git hooks all in one"
   homepage "https://github.com/Roytangrb/gitwok"
