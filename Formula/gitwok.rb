72829abe98eed212bd9713889da90e44cfd8ce48
Roy Tang <roytang.rb@gmail.com>
2021-01-19 15:59:15 +0800

    Release 0.1.7

--- Formula/gitwok.rb
+++ Formula/gitwok.rb
@@ -1,9 +1,9 @@
 class Gitwok < Formula
   desc "Configurable CLI with conventional commits, changelog, git hooks all in one"
   homepage "https://github.com/Roytangrb/gitwok"
-  url "https://github.com/Roytangrb/gitwok/releases/download/v0.1.4/gitwok-mac.tar.gz"
-  sha256 "13b9dbdf99f6cb1c882d7155ce1bad9cedef5acceba9dd0a23b2d88052279303"
-  version "0.1.4"
+  url "https://github.com/Roytangrb/gitwok/archive/v0.1.7.tar.gz"
+  sha256 "a46f8c2117a413de11c1978e4f732163c05bd4580d01f728aa7cdf8115f181ed"
+  version "0.1.7"
 
   def install
     bin.install "gitwok"
