0d9e53273719bf9be9431b3f8ef3e150a9dbb61f
Roy Tang <roytang.rb@gmail.com>
2021-01-22 00:03:59 +0800

    Release 0.2.0

--- Formula/gitwok.rb
+++ Formula/gitwok.rb
@@ -2,9 +2,9 @@ class Gitwok < Formula
   desc "Configurable CLI with conventional commits, changelog, git hooks all in one"
   homepage "https://github.com/Roytangrb/gitwok"
   license "MIT"
-  url "https://github.com/Roytangrb/gitwok/archive/v0.1.7.tar.gz"
-  sha256 "a46f8c2117a413de11c1978e4f732163c05bd4580d01f728aa7cdf8115f181ed"
-  version "0.1.7"
+  url "https://github.com/Roytangrb/gitwok/archive/v0.2.0.tar.gz"
+  sha256 "52c703a1b58e3fca5838e8014ca262480d30787a110e75152d4ad7522b62a6e3"
+  version "0.2.0"
   
   depends_on "go" => :build
 
